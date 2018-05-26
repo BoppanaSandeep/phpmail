72ccf8b7b73364bb4d47ceca49a795d87c7c2389
BoppanaSandeep <boppanasandeep57@gmail.com>
2018-05-26 23:01:19 +0530

    changed for ees.

--- index.php
+++ index.php
@@ -1,39 +1,41 @@
 <?php
+header("Access-Control-Allow-Headers", "*");
+header("Access-Control-Allow-Origin", "*");
 error_reporting(~E_ALL);
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 
 //Load composer's autoloader
 require 'vendor/autoload.php';
 
-echo $name=$_POST['name'];
-echo $email=$_POST['email'];
+$input = json_decode(file_get_contents('php://input'), true);
+$email = $input['email'];
 
-if($email!=''){
-    $mail = new PHPMailer(true);                                // Passing `true` enables exceptions
+if ($email != '') {
+    $mail = new PHPMailer(true); // Passing `true` enables exceptions
     try {
         //Server settings
-        $mail->SMTPDebug = 2;                                   // Enable verbose debug output
-        $mail->isSMTP();                                        // Set mailer to use SMTP
-        $mail->SMTPSecure = 'ssl';                              // Enable TLS encryption, `ssl` also accepted
-        $mail->Host = 'ssl://smtp.gmail.com:465';               // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                                 // Enable SMTP authentication
-        $mail->Username = 'your@gmail.com';          // SMTP username
-        $mail->Password = 'your password';                         // SMTP password
-        $mail->Port = 465;                                      // TCP port to connect to
+        $mail->SMTPDebug = 0; // Enable verbose debug output
+        $mail->isSMTP(); // Set mailer to use SMTP
+        $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
+        $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true; // Enable SMTP authentication
+        $mail->Username = 'eesapplications@gmail.com'; // SMTP username
+        $mail->Password = 'ees.applications'; // SMTP password
+        $mail->Port = 465; // TCP port to connect to
         $mail->SMTPOptions = array(
-                'ssl' => array(
+            'ssl' => array(
                 'verify_peer' => false,
                 'verify_peer_name' => false,
-                'allow_self_signed' => true
-                )
-            );
+                'allow_self_signed' => true,
+            ),
+        );
 
         //Recipients
-        $mail->setFrom('your@gmail.com', 'your name');
-        $mail->addAddress($email, $name);                       // Add a recipient
+        $mail->setFrom('eesappliations@gmail.com', 'ees');
+        $mail->addAddress($email); // Add a recipient
         //$mail->addAddress('ellen@example.com');               // Name is optional
         //$mail->addReplyTo('info@example.com', 'Information');
         //$mail->addCC('cc@example.com');
@@ -44,35 +46,60 @@ if($email!=''){
         //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
         //Content
-        $mail->isHTML(true);                                    // Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = '<b>This is the body in plain text for non-HTML mail clients</b>';
-
-        $mail->send();
-        echo 'Message has been sent';
-        header("Location:index.php?status=Message has been sent");
+        $mail->isHTML(true); // Set email format to HTML
+        $mail->Subject = 'You have received a text message. - ees';
+        $mail->Body = '<div>
+        <div>
+        <h4>Hi,</h4>
+        </div>
+        <p>You have received a message when you are not there at your phone.</p>
+        <table style="width:60%" border="1">
+            <thead>
+                <tr>
+                    <th width="25%">
+                        Contact
+                    </th>
+                    <th width="75%">
+                        Message
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td width="25%">
+                    ' . $input['contact'] . '
+                    </td>
+                    <td width="75%">
+                    ' . $input['msg_body'] . '
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br><br>
+        <div>
+          Thanks for choosing ees,<br>
+          ees.
+        </div>
+      </div>';
+        //$mail->AltBody = '<b>This is the body in plain text for non-HTML mail clients</b>';
+        $result = array();
+        if ($mail->send()) {
+            $result['msg'] = "Email sent successfully.";
+            $result['status'] = 200;
+            echo json_encode($result);
+        } else {
+            $result['msg'] = "Sending email failed.";
+            $result['status'] = 404;
+            echo json_encode($result);
+        }
     } catch (Exception $e) {
-        echo 'Message could not be sent.';
+        $result['msg'] = "Sending email failed.";
+        $result['status'] = 404;
+        echo json_encode($result);
         //echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
+} else {
+    $result['msg'] = "Sending email failed.";
+    $result['status'] = 404;
+    echo json_encode($result);
 }
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Mail</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-</head>
-<body>
-    <?php echo $status=$_REQUEST['status']!=''?$_REQUEST['status']:'' ?>
-    <form action="index.php" method="post" name="mail" id="mail">
-        <input type="text" placeholder="Name" name="name" id="name" value="" />
-        <input type="email" placeholder="Email" name="email" id="email" value="" />
-        <input type="submit" name="submit" id="submit" value="Submit" />
-    </form>
-</body>
-</html>
